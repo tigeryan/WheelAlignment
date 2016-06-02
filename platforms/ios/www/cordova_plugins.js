6d32ce6d1167317c3822d2671d92622d44358c93
John Ceci <jceci@usa.m3.com>
2016-06-02 13:04:32 -0400

    second

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,27 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-gyroscope/www/Orientation.js",
+        "id": "cordova-plugin-gyroscope.Orientation",
+        "pluginId": "cordova-plugin-gyroscope",
+        "clobbers": [
+            "Orientation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-gyroscope/www/gyroscope.js",
+        "id": "cordova-plugin-gyroscope.gyroscope",
+        "pluginId": "cordova-plugin-gyroscope",
+        "clobbers": [
+            "navigator.gyroscope"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-gyroscope": "0.1.4"
 }
 // BOTTOM OF METADATA
 });
--- www/js/index.js
+++ www/js/index.js
@@ -45,6 +45,10 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+
+        var gyroscope = navigator.gyroscope;
+        console.log(gyroscope.getCurrent);
+
     }
 };
 
